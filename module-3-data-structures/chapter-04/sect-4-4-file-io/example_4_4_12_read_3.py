996cf0a0956ae26b2f84b69197fc1c1406a1f033
kyc_james <kayikchoi20@gmail.com>
2024-11-28 08:26:14 +1100

    Update example_4_4_12_read_3.py

--- module-3-data-structures/chapter-04/sect-4-4-file-io/example_4_4_12_read_3.py
+++ module-3-data-structures/chapter-04/sect-4-4-file-io/example_4_4_12_read_3.py
@@ -1,9 +1,29 @@
 # Simple File Reading 3
-# readline() moves the reader forward to the start of the next line, 
-# so when we call readline(), the next line is passed permanently;
-# IMPORTANT:  if we want to store or print the lines without the line breaks ( the empty space in each line in the previous code)
-# we need to strip the whitespace off of them
+# readline() reads and returns an entire line, including the newline character (\n) at the end
+# Each subsequent readline() moves the file pointer to the start of the next line
+# To avoid double spacing when printing, we use strip() to remove the \n that comes from the file
+# (print() will still add its own newline automatically)
 
+inputFile = open("./output/OutputFile.txt", "r")
+
+# Print first line without the file's \n character
+print(inputFile.readline().strip())
+# Print second line without the file's \n character
+print(inputFile.readline().strip())
+# Print third line without the file's \n character
+print(inputFile.readline().strip())
+
+# Output is now
+# 12
+# 23
+# 34
+# instead of
+# 12
+
+# 23
+
+# 34
+# like in 4_4_11
 inputFile = open("./output/OutputFile.txt", "r")
 
 #Print the next line of inputFile
